58b346723f8af336cceec1ee1a04f038dd8d1327
AustinGerald <a.floydgerald@gmail.com>
2019-03-19 10:10:56 -0500

    built gradient component

--- schedule/src/components/home.js
+++ schedule/src/components/home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Library from './library/library';
 import Schedule from './schedule/schedule';
   
-}
+
 
 class Home extends Component {
   render() {
