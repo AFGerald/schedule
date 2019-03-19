c6f2d7fdbc6358096ce186f44acebeb8a9e1244d
AustinGerald <a.floydgerald@gmail.com>
2019-03-19 12:18:09 -0500

    built progress tracker component

--- schedule/src/components/schedule/schedule.js
+++ schedule/src/components/schedule/schedule.js
@@ -10,6 +10,7 @@ class Schedule extends Component {
                 <ScheduleCourse title={'Problem Solving'}/>
                 <ScheduleCourse title={'Problem Solving'}/>
                 <ScheduleCourse title={'Problem Solving'}/>
+                <ProgressTracker/>
                 <Gradient/>
             </div>
         )
